5fdc882e66b4644bac052569c0860cae9b0bb0b1
Kim Lew <kimlew451@gmail.com>
2019-11-10 10:30:09 -0700

    Added -maxdepth 1 to find - prevents re-making subdirs & files under exiting subdirs

--- create_year_month_subdirs_rename_files.sh
+++ create_year_month_subdirs_rename_files.sh
@@ -20,7 +20,7 @@ else
  fi
 
 # Loop that processes entire given directory.
-find "$directory_path" -type f -name '*.jpg' |
+find "$directory_path" -maxdepth 1 -type f -name '*.jpg' |
 while read a_file_name; do
   exif_date="$(identify -format '%[EXIF:DateTimeOriginal]' "$a_file_name")"
   # echo $exif_date
